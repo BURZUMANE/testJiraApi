380535727462ed343bae73b02d964ab40abb71b4
Timur Zakirov <timurzakirov@Timurs-MacBook-Air.local>
2020-07-01 23:46:41 +0300

    delta

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require("express");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
 const cors = require("cors");
 const getIssues = require("./services/getIssues");
-// console.log(getIssues().then(data => console.log(data)));
+const byFilter = require("./services/byFilter");
 const app = express();
-app.use(morgan("dev"));
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" },
+);
+
+dotenv.config({ path: path.join(__dirname, ".env") });
+// setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(cors({ origin: "*", methods: ["GET"] }));
 
-app.get("/api/issues", async (req, res) => {
-  const issues = await getIssues();
+app.get("/api/issues/:jql", async (req, res) => {
+  const jql = req.params.jql;
+  const issues = await getIssues(jql);
   return res.json(issues);
 });
 
-const PORT = process.env.PORT || 5002;
+app.get("/api/sepcified/:jql", async (req, res) => {
+  const jql = req.params.jql;
+  const issues = await byFilter(jql);
+  return res.json(issues);
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`),
 );
--- services/getIssues.js
+++ services/getIssues.js
@@ -2,17 +2,120 @@
 // https://test-fast.atlassian.net
 // This code sample uses the 'node-fetch' library:
 // https://www.npmjs.com/package/node-fetch
-const express = require("express");
 const fetch = require("node-fetch");
-const initialObj = {
-  BACKLOG: { Medium: [], Low: [], Lowest: [], High: [], Highest: [] },
-  SFD: { Medium: [], Low: [], Lowest: [], High: [], Highest: [] },
-  PROGRESS: { Medium: [], Low: [], Lowest: [], High: [], Highest: [] },
-  DONE: { Medium: [], Low: [], Lowest: [], High: [], Highest: [] },
+const _ = require("lodash");
+const initialObject = {
+  BACKLOG: {
+    Bug: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Task: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Epic: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Story: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+  },
+  SFD: {
+    Bug: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Task: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Epic: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Story: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+  },
+  PROGRESS: {
+    Bug: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Task: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Epic: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Story: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+  },
+  DONE: {
+    Bug: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Task: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Epic: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+    Story: {
+      Highest: { jql: "", content: [] },
+      High: { jql: "", content: [] },
+      Medium: { jql: "", content: [] },
+      Low: { jql: "", content: [] },
+    },
+  },
 };
 
 const getPriority = item => {
   return item.fields.priority.name;
+};
+const getTaskType = item => {
+  return item.fields.issuetype.name;
 };
 
 const getStatus = statusName => {
@@ -21,7 +124,7 @@ const getStatus = statusName => {
   } else if (statusName === "Selected for Development") {
     return "SFD";
   } else if (statusName === "In Progress") {
-    return PROGRESS;
+    return "PROGRESS";
   } else {
     return "DONE";
   }
@@ -32,21 +135,17 @@ const makeSingleIssue = item => {
     name: item.fields.summary,
     created: item.fields.created,
     duedate: item.fields.duedate,
+    priority: item.fields.priority.name,
   };
 };
-const getIssues = () => {
-  return fetch(
-    "https://test-fast.atlassian.net/rest/api/3/search?jql=issuetype+in+standardIssueTypes()+ORDER+BY+created+DESC",
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          "1234tima.g@gmail.com:bOuhKUmKZopzvL90hPjd07EC",
-        ).toString("base64")}`,
-        Accept: "application/json",
-      },
+const getIssues = jql => {
+  return fetch(`https://test-fast.atlassian.net/rest/api/3/search?jql=${jql}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Basic ${Buffer.from(process.env.KEY).toString("base64")}`,
+      Accept: "application/json",
     },
-  )
+  })
     .then(response => {
       console.log(`Response: ${response.status} ${response.statusText}`);
       return response.json();
@@ -59,27 +158,36 @@ const getIssues = () => {
         if (!item.fields.assignee) {
           displayName = "unassigned";
         } else {
-          console.log(item.fields.assignee.displayName);
-          displayName = item.fields.assignee.displayName;
+          displayName = item.fields.assignee.displayName.split(" ").join("");
         }
-        if (!assignees.displayName) {
-          assignees[displayName] = initialObj;
+        if (!(displayName in assignees)) {
+          assignees[displayName] = _.cloneDeep(initialObject);
+          // assignees[displayName].accountId = item.fields.assignee.accountId;
+        } else {
         }
-
-        const singleIssue = makeSingleIssue(item);
+        let accountId;
+        if (!item.fields.assignee) {
+          accountId = "EMPTY";
+        } else {
+          accountId = item.fields.assignee.accountId;
+        }
+        const singleIssue = makeSingleIssue(item)
+          ? makeSingleIssue(item)
+          : null;
         const statusName = item.fields.status.name;
         const resStatus = getStatus(statusName);
         const priority = getPriority(item);
-          console.log(statusName)
-          console.log(priority)
-          console.log(singleIssue);
-          console.log(idx);
-        // assignees[displayName][resStatus][priority].push(singleIssue);
-        // console.log(assignees);
+        const taskType = getTaskType(item);
+        const jqlStr = `issuetype%20%3D%20${taskType}%20AND%20assignee%20in%20(${accountId})%20AND%20status%20%3D%20${statusName}%20AND%20priority%20%3D%20${priority}`;
+        assignees[displayName][resStatus][taskType][priority]["jql"] = jqlStr;
+        assignees[displayName][resStatus][taskType][priority]["content"].push(
+          singleIssue,
+        );
       });
 
       return assignees;
     })
     .catch(err => console.error(err));
 };
+
 module.exports = getIssues;
