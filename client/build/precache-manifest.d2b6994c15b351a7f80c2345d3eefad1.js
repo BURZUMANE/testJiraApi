2005ed14f2492eb21017d977f281c9975d0d146a
Timur Zakirov <timurzakirov@Timurs-MacBook-Air.local>
2020-07-02 01:51:08 +0300

    delta

---
+++ client/build/precache-manifest.d2b6994c15b351a7f80c2345d3eefad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3c1a184e542b33aab8ad9cac50bd59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b12385f8ec3208a0ff",
+    "url": "/static/css/main.0f85b6f1.chunk.css"
+  },
+  {
+    "revision": "9fae9ff6836eeb18aae0",
+    "url": "/static/js/2.05adf54c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.05adf54c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b12385f8ec3208a0ff",
+    "url": "/static/js/main.5f9b2280.chunk.js"
+  },
+  {
+    "revision": "a4c3f6ac9c8f337967f2",
+    "url": "/static/js/runtime-main.c8b1a502.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,35 +1,9 @@
 const express = require("express");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-const fs = require("fs");
-const path = require("path");
-const cors = require("cors");
-const getIssues = require("./services/getIssues");
-const byFilter = require("./services/byFilter");
 const app = express();
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "access.log"),
-  { flags: "a" },
-);
+const path = require("path");
+const PORT = 8080;
 
-dotenv.config({ path: path.join(__dirname, ".env") });
-// setup the logger
-app.use(morgan("combined", { stream: accessLogStream }));
-app.use(cors({ origin: "*", methods: ["GET"] }));
-
-app.get("/api/issues/:jql", async (req, res) => {
-  const jql = req.params.jql;
-  const issues = await getIssues(jql);
-  return res.json(issues);
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.listen(PORT, () => {
+  console.log(`server on port ${PORT}`);
 });
-
-app.get("/api/sepcified/:jql", async (req, res) => {
-  const jql = req.params.jql;
-  const issues = await byFilter(jql);
-  return res.json(issues);
-});
-
-const PORT = process.env.PORT;
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`),
-);
